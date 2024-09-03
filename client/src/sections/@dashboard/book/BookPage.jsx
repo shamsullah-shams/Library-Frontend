0e23ae38b748b814c68e026cbf09ef2353c1b2d0
shamsullah-shams <shamsullah07073@gmail.com>
2024-09-03 18:13:07 +0430

    Search Bar added

--- client/src/sections/@dashboard/book/BookPage.jsx
+++ client/src/sections/@dashboard/book/BookPage.jsx
@@ -17,9 +17,10 @@ import {
   Typography,
 } from '@mui/material';
 import { Alert } from '@mui/lab';
-import { styled } from '@mui/material/styles';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
 import { useAuth } from '../../../hooks/useAuth';
-
 import Label from '../../../components/label';
 import BookDialog from './BookDialog';
 import BookForm from './BookForm';
@@ -35,6 +36,45 @@ const StyledBookImage = styled('img')({
   objectFit: 'cover',
   position: 'absolute',
 });
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    border: '1px solid #f0efef',
+    borderRadius: '20px',
+  },
+}));
 
 const BookPage = () => {
   const { user, tokens } = useAuth();
@@ -195,6 +235,15 @@ const BookPage = () => {
     setIsModalOpen(false);
   };
 
+  const previewBook = () => {
+    const selectedBook = books.find((element) => element.id === selectedBookId);
+    if (selectedBook) {
+      window.open(`http://localhost:5000/images/${selectedBook?.pdf}`, '_blank');
+    } else {
+      alert('something went wrong');
+    }
+  };
+
   return (
     <>
       <Helmet>
@@ -202,7 +251,7 @@ const BookPage = () => {
       </Helmet>
 
       <Container>
-        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+        <Stack direction="row" alignItems="center" justifyContent="space-between">
           <Typography variant="h3" sx={{ mb: 5 }}>
             Books
           </Typography>
@@ -219,6 +268,14 @@ const BookPage = () => {
             </Button>
           )}
         </Stack>
+        <Container sx={{ mb: 5 }}>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} style={{ width: '100%' }} />
+          </Search>
+        </Container>
 
         {isTableLoading ? (
           <Grid padding={2} style={{ textAlign: 'center' }}>
@@ -299,6 +356,10 @@ const BookPage = () => {
           },
         }}
       >
+        <MenuItem onClick={previewBook}>
+          <Iconify icon={'eva:eye-outline'} sx={{ mr: 2 }} />
+          Preview
+        </MenuItem>
         <MenuItem
           onClick={() => {
             setIsUpdateForm(true);
